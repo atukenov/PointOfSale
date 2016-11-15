3972ec38a4d96172cce74b47037c63466f2b7071
atukenov <almaz.t97@gmail.com>
2016-11-15 12:08:02 -0600

    Output List added

---
+++ Project/src/Sales.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AlmazTukenov
+ */
+public class Sales {
+    
+    private String name = "";
+    private int quantity = 0;
+    private int price = 0;
+    
+    public Sales(String name, int quantity, int price)
+    {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return name + "\t" + quantity + "\t" + price + "\n";
+    }
+}
